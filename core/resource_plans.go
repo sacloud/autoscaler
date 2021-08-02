0548bc95b29bc1f0771bf47c036fb0028cef699b
Kazumichi Yamamoto <yamamoto.febc@gmail.com>
2021-08-02 15:28:59 +0900

    Fixed: calculate desired plan

--- core/resource_plans.go
+++ core/resource_plans.go
@@ -21,7 +21,7 @@ import (
 	"github.com/sacloud/autoscaler/defaults"
 )
 
-// ResourcePlan 垂直スケールをサポートするリソースでのプランを表すインターフェース
+// ResourcePlan オートスケールをサポートするリソースでのプランを表すインターフェース
 type ResourcePlan interface {
 	// PlanName プラン名、Configurationで任意のプランに任意の名前をつけるために利用する
 	PlanName() string
@@ -33,7 +33,7 @@ type ResourcePlan interface {
 	LessThanPlan(plans ResourcePlan) bool
 }
 
-// ResourcePlans 垂直スケールをサポートするリソースでのプラン一覧を表すインターフェース
+// ResourcePlans オートスケールをサポートするリソースでのプラン一覧を表すインターフェース
 type ResourcePlans []ResourcePlan
 
 // Sort ResourcePlan.LessThanPlanを用いて昇順にソートする
@@ -58,6 +58,15 @@ func (p *ResourcePlans) Next(resource interface{}) ResourcePlan {
 		return nil
 	}
 
+	if !plans.within(resource) {
+		// resourceがplansの最小より小さい値を持つ場合
+		// 例: plans == [2,4,8], resource == 1 の場合、plansの最初(plans=2)を返す
+		if !plans[0].LessThan(resource) {
+			return plans[0]
+		}
+		return nil
+	}
+
 	next := false
 	for _, plan := range plans {
 		if plan.Equals(resource) || plan.LessThan(resource) {
@@ -68,6 +77,7 @@ func (p *ResourcePlans) Next(resource interface{}) ResourcePlan {
 			return plan
 		}
 	}
+
 	return nil
 }
 
@@ -84,6 +94,16 @@ func (p *ResourcePlans) Prev(resource interface{}) ResourcePlan {
 		return nil
 	}
 
+	if !plans.within(resource) {
+		// resourceがplansの最大より大きな値を持つ場合
+		// 例: plans == [2,4,8], resource == 10 の場合、plansの最後(plans=8)を返す
+		plan := plans[len(plans)-1]
+		if !plan.Equals(resource) && plan.LessThan(resource) {
+			return plan
+		}
+		return nil
+	}
+
 	var prev ResourcePlan
 	for i, plan := range plans {
 		if i > 0 && (plan.Equals(resource) || !plan.LessThan(resource)) {
@@ -95,6 +115,27 @@ func (p *ResourcePlans) Prev(resource interface{}) ResourcePlan {
 		return nil
 	}
 	return prev
+}
+
+func (p *ResourcePlans) within(resource interface{}) bool {
+	plans := *p
+	switch len(plans) {
+	case 0:
+		return false
+	case 1:
+		return plans[0].Equals(resource)
+	default:
+		first := plans[0]
+		last := plans[len(plans)-1]
+		// first <= resource <= last であればtrue
+		if !(first.Equals(resource) || first.LessThan(resource)) {
+			return false
+		}
+		if !last.Equals(resource) && last.LessThan(resource) {
+			return false
+		}
+		return true
+	}
 }
 
 func desiredPlan(ctx *RequestContext, current interface{}, plans ResourcePlans) (ResourcePlan, error) {
