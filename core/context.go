ba75c5c34581610a1a905d4bade25fae715c70aa
Kazumichi Yamamoto <yamamoto.febc@gmail.com>
2022-02-08 14:28:13 +0900

    Added loadConfigContext struct

---
+++ core/context.go
@@ -0,0 +1,64 @@
+// Copyright 2021-2022 The sacloud/autoscaler Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package core
+
+import (
+	"context"
+	"time"
+)
+
+var (
+	_ LoadConfigHolder = &loadConfigContext{}
+	_ context.Context  = &loadConfigContext{}
+)
+
+// loadConfigContext コンフィグのロードオプションを保持するcontext.Context実装
+type loadConfigContext struct {
+	parent context.Context
+	strict bool
+}
+
+// LoadConfigHolder コンフィグのロードオプションを保持しているかを示すインターフェース
+type LoadConfigHolder interface {
+	StrictMode() bool
+}
+
+func newLoadConfigContext(ctx context.Context, strict bool) context.Context {
+	return &loadConfigContext{parent: ctx, strict: strict}
+}
+
+func (c *loadConfigContext) StrictMode() bool {
+	return c.strict
+}
+
+// Deadline context.Context実装
+func (c *loadConfigContext) Deadline() (time.Time, bool) {
+	return c.parent.Deadline()
+}
+
+// Done context.Context実装
+func (c *loadConfigContext) Done() <-chan struct{} {
+	return c.parent.Done()
+}
+
+// Err context.Context実装
+func (c *loadConfigContext) Err() error {
+	return c.parent.Err()
+}
+
+// Value context.Context実装
+func (c *loadConfigContext) Value(key interface{}) interface{} {
+	return c.parent.Value(key)
+}
